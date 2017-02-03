55f5ca5b631b904804340e5768afc1b1b3744a27
chrissickler <christopher.sickler@utexas.edu>
2017-02-03 09:35:11 -0600

    Adding in JARs

---
+++ src/main/java/blog/Servlet.java
@@ -0,0 +1,16 @@
+package blog;
+
+import com.google.appengine.api.users.User;
+import com.google.appengine.api.users.UserService;
+import com.google.appengine.api.users.UserServiceFactory;
+import java.io.IOException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+public class Servlet extends HttpServlet{
+
+	
+	
+}
